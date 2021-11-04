0ef098dc0d45331942218b9358e9a100d4b3d451
Pedro Pereira <j.pedro.diniz1@gmail.com>
2021-11-04 19:29:32 +0000

    Added bot commands with preffix enabled

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ MIN = 5
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
-bot = Bot("!")
+bot = Bot(command_prefix="$")
 
 link_list = ["https://media.giphy.com/media/l3V0j3ytFyGHqiV7W/giphy.gif", "https://media.giphy.com/media/63MO9LTRoTXQk/giphy.gif", "https://media.giphy.com/media/ikcJ56KAyhm8w/giphy.gif", "https://media.giphy.com/media/mFulmRSjkW9by/giphy.gif", "https://media.giphy.com/media/iNLyKccqVmiBy/giphy.gif", "https://media.giphy.com/media/kz6FtjoEVsNTlLnMXz/giphy.gif",
 "https://media.giphy.com/media/bxd1GzWN0v7wY/giphy.gif", "https://media.giphy.com/media/2oeyTMKbIZsHK/giphy.gif", "https://media.giphy.com/media/kbQVMaf2s20f8gSKDm/giphy.gif"]
@@ -32,6 +32,24 @@ async def start():
 async def vkick(self, ctx, target_member: discord.Member):
     """Kicks a Member from a voice channel"""
     await target_member.move_to(None)
+
+@bot.command()
+async def bot_join(ctx, *, channel: discord.VoiceClient = None): # TAKING ARGUMENT CHANNEL SO PPL CAN MAKE THE BOT JOIN A VOICE CHANNEL THAT THEY ARE NOT IN
+    """Joins a voice channel."""
+    destination = channel
+    await destination.connect() # CONNECTING TO DESTINATION
+    # await ctx.send(f"Succesfully joined the voice channel: {destination.name} ({destination.id}).")
+
+#@client.command(name='join',aliases = ['summon']) # CREATING COMMAND "JOIN" WITH ALIAS SUMMON
+#async def bot_join(ctx, *, channel: discord.VoiceChannel = None): # TAKING ARGUMENT CHANNEL SO PPL CAN MAKE THE BOT JOIN A VOICE CHANNEL THAT THEY ARE NOT IN
+   # """Joins a voice channel."""
+   # destination = channel if channel else ctx.author.voice.channel # CHOOSING THE DESTINATION, MIGHT BE THE REQUESTED ONE, BUT IF NOT THEN WE PICK AUTHORS VOICE CHANNEL
+   # if ctx.voice_client: # CHECKING IF THE BOT IS PLAYING SOMETHING
+       # await ctx.voice_state.voice.move_to(destination) # IF THE BOT IS PLAYING WE JUST MOVE THE BOT TO THE DESTINATION
+   #     return
+
+   # await destination.connect() # CONNECTING TO DESTINATION
+   # await ctx.send(f"Succesfully joined the voice channel: {destination.name} ({destination.id}).")
  
 @client.event
 async def on_message(message):
@@ -39,6 +57,32 @@ async def on_message(message):
     return
   if message.content.startswith('$kick'):
     await message.channel.send('Sempre no disconnect! A dar shades desde que o VG nasceu')
+
+@bot.command(name="join")
+async def join(ctx):
+    author = ctx.message.author
+    channel = author.voice.channel
+    await channel.connect()
+    print("i'm in the voice channel")
+
+
+@bot.command(name="leave")
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+    print("i'm out of the voice channel")
+
+
+#@bot.command(name="join")
+#async def join(ctx):
+#    channel = ctx.author.voice.channel
+#    voice = get(self.bot.voice_clients, guild=ctx.guild)
+#    if voice and voice.is_connected():
+#        await voice.move_to(channel)
+#    else:
+#        voice = await channel.connect()
+#@bot.command(name="leave")
+#async def leave(ctx):
+#    await ctx.voice_client.disconnect()
 
 async def retrieve_active_voice_channel():
       """Scans all active voice channels the bot can see and returns one"""
@@ -56,8 +100,8 @@ async def retrieve_active_voice_channel():
               # print("print VoiceChannel Members", VoiceChannel.members)
               # xhannel = text_channel_list.append(channel)
               print("print Voice Channel Voice states", VoiceChannel.voice_states )
-              print("channel id")
               channel = client.get_channel(VoiceChannel.id) 
+              print("print channel id",channel.id)
               print("print channel name",channel.name)
               #gets the channel you want to get the list from
               print("associate members in specific channel")
@@ -70,14 +114,29 @@ async def retrieve_active_voice_channel():
                   print("member:", member)
                   print("member id:", member.id)
                   print("member name:", member.name)
+                  print("channel id:", channel.id)
+                  print("channel name:", channel.name)
+                  # print("bot name:", client.name)
+                  # print("bot id:", client.id)
                   states = channel.voice_states
                   strikes = [states[memberids]]
                   strikes0 = strikes[0]
                   print(strikes0)
                   while re.search("self_mute=True self_deaf=True", str(strikes0)):
                   # if re.search("self_mute=True self_deaf=True", str(strikes0)):
+                    #await bot_join(channel.id)
+                    #print('Bot joined the channel.')
+                    #voice = channel.id
+                    # bot.connect(channel.id)
+                    #xclientconnect = channel.id
+                    #await xclientconnect.move_to()
+                    # channel.id.move_to()
+                    #{.author}
+                    #await join(member)
                     print("kick")
                     await vkick(None,None,member)
+                    # user = client.get_user(memberids)
+                    #await user.send('Olá, foste kickado por estares Deafen. Estás a ser alvo de uma experiencia social')
                     # Still need to fix this as a indepedent config value
                     # right channel
                     channelget = client.get_channel(863030632846589983)
@@ -88,6 +147,8 @@ async def retrieve_active_voice_channel():
                     embed.set_footer(text="Do: "+str(channel))
                     embed.set_image(url=random.choice(link_list))
                     await channelget.send(embed=embed)
+                    #await xclientconnect.voice_client.disconnect()
+                    #await leave()
                     return
               print("finish after this")
 
